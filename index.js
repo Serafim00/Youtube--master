aa92d6e3f34fab27d7266544293a9406b8c2b4f1
Serafim00 <57284913+Serafim00@users.noreply.github.com>
2021-01-13 21:32:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "youtubeGame";
+const gameName = "testgame";
 const queries = {};
 server.use(express.static(path.join(__dirname, 'YoutubeTestGame')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot implements a T-Rex jumping game. Say /game if you want to play."));
